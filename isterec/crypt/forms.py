48278a52eb814a2ae9b87a0eb8fc098f6e964925
shiv4896 <shivshnkr420@gmail.com>
2016-06-26 00:29:43 +0300

    v1

--- isterec/crypt/forms.py
+++ isterec/crypt/forms.py
@@ -11,8 +11,8 @@ class UploadFileForm(forms.ModelForm):
 
 	def __init__(self, *args, **kwargs):
                 super(UploadFileForm, self).__init__(*args, **kwargs)
-                self.fields['name'].label     = "Your name     :"
-                self.fields['email'].label    = "Your E-mail   :"
-                self.fields['rollno'].label   = "Your Roll No  :"
-                self.fields['mobileno'].label = "Your Mobile No:"
+                self.fields['name'].label     = "Your name"
+                self.fields['email'].label    = "Your E-mail"
+                self.fields['rollno'].label   = "Your Roll No"
+                self.fields['mobileno'].label = "Your Mobile No"
                 self.fields['body'].label     = "What do you want to join crypt?"
