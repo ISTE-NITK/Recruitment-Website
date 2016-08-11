078d075cb92841ab9170e85d4ff2abe24631b320
shiv4896 <shivshnkr420@gmail.com>
2016-08-11 17:07:29 +0530

    update settings file

--- isterec/isterec/settings.py
+++ isterec/isterec/settings.py
@@ -30,7 +30,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['isterec.herokuapp.com']
 
-
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com' 
+EMAIL_HOST_USER = 'user'
+EMAIL_HOST_PASSWORD = 'password'
+EMAIL_PORT = 587
+DEFAULT_FROM_EMAIL = 'user'
 
 AWS_STORAGE_BUCKET_NAME = 'AWS_STORAGE_BUCKET_NAME'
 AWS_ACCESS_KEY_ID = 'AWS_ACCESS_KEY_ID'
