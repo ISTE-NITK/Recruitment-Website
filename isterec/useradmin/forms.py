e263b73a5dcf4a7145b8241256414f56823cdc56
shiv4896 <shivshnkr420@gmail.com>
2016-08-22 12:51:48 +0530

    changed score display to 2 forms

--- isterec/useradmin/forms.py
+++ isterec/useradmin/forms.py
@@ -56,10 +56,94 @@ class RegistrationForm(forms.ModelForm):
                 raise forms.ValidationError(_("The two password fields did not match."))
         return self.cleaned_data
 
+class CryptReviewForm(forms.ModelForm):
+	class Meta:
+		model = CryptRecData
+		fields = ('score','is_selected','reviewer_1','reviewer_2','reviewer_3')
+
+	def __init__(self, *args, **kwargs):
+                super(CryptReviewForm, self).__init__(*args, **kwargs)
+                self.fields['is_selected'].label = "Selected for next round?"
+                self.fields['reviewer_1'].label = "Panel Member 1(not HR)"
+                self.fields['reviewer_2'].label = "Panel Member 2(not HR)"
+                self.fields['reviewer_3'].label = "Panel Member HR"
+
+class ChargeReviewForm(forms.ModelForm):
+	class Meta:
+		model = ChargeRecData
+		fields = ('score','is_selected','reviewer_1','reviewer_2','reviewer_3')
+
+	def __init__(self, *args, **kwargs):
+                super(ChargeReviewForm, self).__init__(*args, **kwargs)
+                self.fields['is_selected'].label = "Selected for next round?"
+                self.fields['reviewer_1'].label = "Panel Member 1(not HR)"
+                self.fields['reviewer_2'].label = "Panel Member 2(not HR)"
+                self.fields['reviewer_3'].label = "Panel Member HR"
+
+class ClutchReviewForm(forms.ModelForm):
+	class Meta:
+		model = ClutchRecData
+		fields = ('score','is_selected','reviewer_1','reviewer_2','reviewer_3')
+
+	def __init__(self, *args, **kwargs):
+                super(ClutchReviewForm, self).__init__(*args, **kwargs)
+                self.fields['is_selected'].label = "Selected for next round?"
+                self.fields['reviewer_1'].label = "Panel Member 1(not HR)"
+                self.fields['reviewer_2'].label = "Panel Member 2(not HR)"
+                self.fields['reviewer_3'].label = "Panel Member HR"
+
+class CreditReviewForm(forms.ModelForm):
+	class Meta:
+		model = CreditRecData
+		fields = ('score','is_selected','reviewer_1','reviewer_2','reviewer_3')
+
+	def __init__(self, *args, **kwargs):
+                super(CreditReviewForm, self).__init__(*args, **kwargs)
+                self.fields['is_selected'].label = "Selected for next round?"
+                self.fields['reviewer_1'].label = "Panel Member 1(not HR)"
+                self.fields['reviewer_2'].label = "Panel Member 2(not HR)"
+                self.fields['reviewer_3'].label = "Panel Member HR"
+
+class CreateReviewForm(forms.ModelForm):
+	class Meta:
+		model = CreateRecData
+		fields = ('score','is_selected','reviewer_1','reviewer_2','reviewer_3')
+
+	def __init__(self, *args, **kwargs):
+                super(CreateReviewForm, self).__init__(*args, **kwargs)
+                self.fields['is_selected'].label = "Selected for next round?"
+                self.fields['reviewer_1'].label = "Panel Member 1(not HR)"
+                self.fields['reviewer_2'].label = "Panel Member 2(not HR)"
+                self.fields['reviewer_3'].label = "Panel Member HR"
+
+class ChronicleReviewForm(forms.ModelForm):
+	class Meta:
+		model = ChronicleRecData
+		fields = ('score','is_selected','reviewer_1','reviewer_2','reviewer_3')
+
+	def __init__(self, *args, **kwargs):
+                super(ChronicleReviewForm, self).__init__(*args, **kwargs)
+                self.fields['is_selected'].label = "Selected for next round?"
+                self.fields['reviewer_1'].label = "Panel Member 1(not HR)"
+                self.fields['reviewer_2'].label = "Panel Member 2(not HR)"
+                self.fields['reviewer_3'].label = "Panel Member HR"
+
+class CivilReviewForm(forms.ModelForm):
+	class Meta:
+		model = CivilRecData
+		fields = ('score','is_selected','reviewer_1','reviewer_2','reviewer_3')
+
+	def __init__(self, *args, **kwargs):
+                super(CivilReviewForm, self).__init__(*args, **kwargs)
+                self.fields['is_selected'].label = "Selected for next round?"
+                self.fields['reviewer_1'].label = "Panel Member 1(not HR)"
+                self.fields['reviewer_2'].label = "Panel Member 2(not HR)"
+                self.fields['reviewer_3'].label = "Panel Member HR"
+
 class CryptScoreForm(forms.ModelForm):
 	class Meta:
 		model = CryptRecData
-		fields = ('score','is_selected',)
+		fields = ('is_selected',)
 
 	def __init__(self, *args, **kwargs):
                 super(CryptScoreForm, self).__init__(*args, **kwargs)
@@ -68,7 +152,7 @@ class CryptScoreForm(forms.ModelForm):
 class ChargeScoreForm(forms.ModelForm):
 	class Meta:
 		model = ChargeRecData
-		fields = ('score','is_selected',)
+		fields = ('is_selected',)
 
 	def __init__(self, *args, **kwargs):
                 super(ChargeScoreForm, self).__init__(*args, **kwargs)
@@ -77,7 +161,7 @@ class ChargeScoreForm(forms.ModelForm):
 class ClutchScoreForm(forms.ModelForm):
 	class Meta:
 		model = ClutchRecData
-		fields = ('score','is_selected',)
+		fields = ('is_selected',)
 
 	def __init__(self, *args, **kwargs):
                 super(ClutchScoreForm, self).__init__(*args, **kwargs)
@@ -86,7 +170,7 @@ class ClutchScoreForm(forms.ModelForm):
 class CreditScoreForm(forms.ModelForm):
 	class Meta:
 		model = CreditRecData
-		fields = ('score','is_selected',)
+		fields = ('is_selected',)
 
 	def __init__(self, *args, **kwargs):
                 super(CreditScoreForm, self).__init__(*args, **kwargs)
@@ -95,7 +179,7 @@ class CreditScoreForm(forms.ModelForm):
 class CreateScoreForm(forms.ModelForm):
 	class Meta:
 		model = CreateRecData
-		fields = ('score','is_selected',)
+		fields = ('is_selected',)
 
 	def __init__(self, *args, **kwargs):
                 super(CreateScoreForm, self).__init__(*args, **kwargs)
@@ -104,7 +188,7 @@ class CreateScoreForm(forms.ModelForm):
 class ChronicleScoreForm(forms.ModelForm):
 	class Meta:
 		model = ChronicleRecData
-		fields = ('score','is_selected',)
+		fields = ('is_selected',)
 
 	def __init__(self, *args, **kwargs):
                 super(ChronicleScoreForm, self).__init__(*args, **kwargs)
@@ -113,7 +197,7 @@ class ChronicleScoreForm(forms.ModelForm):
 class CivilScoreForm(forms.ModelForm):
 	class Meta:
 		model = CivilRecData
-		fields = ('score','is_selected',)
+		fields = ('is_selected',)
 
 	def __init__(self, *args, **kwargs):
                 super(CivilScoreForm, self).__init__(*args, **kwargs)
